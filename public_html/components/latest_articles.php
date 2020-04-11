766261fafe500dd6eed07623baa5654975243663
Joshua Blokland <shua.blokland@gmail.com>
2020-04-11 12:46:02 +0200

    GRid

---
+++ public_html/components/latest_articles.php
@@ -0,0 +1,12 @@
+<?php
+function latest_articles() {
+    return '
+        <div class="container">
+            <div class"row">
+                <div class="col-2">
+                </div>
+            </div>
+        </div>
+    ';
+}
+?>
--- public_html/index.php
+++ public_html/index.php
@@ -1,10 +1,11 @@
 <?php
-include $_SERVER['DOCUMENT_ROOT'] . '/components/header.php';
-include $_SERVER['DOCUMENT_ROOT'] . '/components/footer.php';
+    include $_SERVER['DOCUMENT_ROOT'] . '/components/header.php';
+    include $_SERVER['DOCUMENT_ROOT'] . '/components/footer.php';
+    include $_SERVER['DOCUMENT_ROOT'] . '/components/latest_articles.php';
 ?>
 
 <?php echo nativedsd_header(); ?>
 
-NativeDSD
+<?php echo latest_articles(); ?>
 
 <?php echo nativedsd_footer(); ?>
