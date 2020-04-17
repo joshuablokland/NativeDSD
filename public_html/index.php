6875451a56653f64024b87d5dacadf10b406ff4d
Joshua Blokland <shua.blokland@gmail.com>
2020-04-17 15:55:28 +0200

    Update structure, add a lib folder

--- public_html/index.php
+++ public_html/index.php
@@ -1,29 +1,29 @@
 <?php
     // Helpers
-    include $_SERVER['DOCUMENT_ROOT'] . '/helpers/url.php';
+    include $_SERVER['DOCUMENT_ROOT'] . '/lib/helpers/url.php';
 
     // Components
-    include $_SERVER['DOCUMENT_ROOT'] . '/components/header.php';
-    include $_SERVER['DOCUMENT_ROOT'] . '/components/footer.php';
-    include $_SERVER['DOCUMENT_ROOT'] . '/components/title.php';
-    include $_SERVER['DOCUMENT_ROOT'] . '/components/menu-items.php';
-    include $_SERVER['DOCUMENT_ROOT'] . '/components/post_meta.php';
-    include $_SERVER['DOCUMENT_ROOT'] . '/components/subscribe.php';
-    include $_SERVER['DOCUMENT_ROOT'] . '/components/articles/article.php';
-    include $_SERVER['DOCUMENT_ROOT'] . '/components/articles/aricle-hero.php';
-    include $_SERVER['DOCUMENT_ROOT'] . '/components/music-reviews/music-review.php';
-    include $_SERVER['DOCUMENT_ROOT'] . '/components/recordings/recording-hero.php';
-    include $_SERVER['DOCUMENT_ROOT'] . '/components/recordings/recording.php';
-    include $_SERVER['DOCUMENT_ROOT'] . '/components/hardware-updates/hardware-update-hero.php';
+    include $_SERVER['DOCUMENT_ROOT'] . '/lib/components/header.php';
+    include $_SERVER['DOCUMENT_ROOT'] . '/lib/components/footer.php';
+    include $_SERVER['DOCUMENT_ROOT'] . '/lib/components/title.php';
+    include $_SERVER['DOCUMENT_ROOT'] . '/lib/components/menu-items.php';
+    include $_SERVER['DOCUMENT_ROOT'] . '/lib/components/post_meta.php';
+    include $_SERVER['DOCUMENT_ROOT'] . '/lib/components/subscribe.php';
+    include $_SERVER['DOCUMENT_ROOT'] . '/lib/components/articles/article.php';
+    include $_SERVER['DOCUMENT_ROOT'] . '/lib/components/articles/aricle-hero.php';
+    include $_SERVER['DOCUMENT_ROOT'] . '/lib/components/music-reviews/music-review.php';
+    include $_SERVER['DOCUMENT_ROOT'] . '/lib/components/recordings/recording-hero.php';
+    include $_SERVER['DOCUMENT_ROOT'] . '/lib/components/recordings/recording.php';
+    include $_SERVER['DOCUMENT_ROOT'] . '/lib/components/hardware-updates/hardware-update-hero.php';
     
     
     // Page components
-    include $_SERVER['DOCUMENT_ROOT'] . '/pages/home/latest-articles.php';
-    include $_SERVER['DOCUMENT_ROOT'] . '/pages/home/news.php';
-    include $_SERVER['DOCUMENT_ROOT'] . '/pages/home/music-reviews.php';
-    include $_SERVER['DOCUMENT_ROOT'] . '/pages/home/recording-reports.php';
-    include $_SERVER['DOCUMENT_ROOT'] . '/pages/home/hardware-updates.php';
-    include $_SERVER['DOCUMENT_ROOT'] . '/pages/home/highlighted-articles.php';
+    include $_SERVER['DOCUMENT_ROOT'] . '/lib/pages/home/latest-articles.php';
+    include $_SERVER['DOCUMENT_ROOT'] . '/lib/pages/home/news.php';
+    include $_SERVER['DOCUMENT_ROOT'] . '/lib/pages/home/music-reviews.php';
+    include $_SERVER['DOCUMENT_ROOT'] . '/lib/pages/home/recording-reports.php';
+    include $_SERVER['DOCUMENT_ROOT'] . '/lib/pages/home/hardware-updates.php';
+    include $_SERVER['DOCUMENT_ROOT'] . '/lib/pages/home/highlighted-articles.php';
     
 ?>
 
